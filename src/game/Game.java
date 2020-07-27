eb79644785be61257b958bad725ce76288d769a7
Javier Gonzalez <gonzalezjavier925@gmail.com>
2020-07-27 13:44:34 -0700

    Updated map to bigger size. Added in walls to the world and created a split screen and minimap.

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,12 +1,19 @@
 package game;
 
+import game.GameObjects.BreakableWall;
 import game.GameObjects.Tank;
+import game.GameObjects.UnbreakableWall;
+import game.GameObjects.Wall;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Objects;
 
 import static javax.imageio.ImageIO.read;
@@ -17,9 +24,7 @@ public class Game extends JPanel implements Runnable {
     private Tank tank1;
     private Tank tank2;
     private Launcher launcher;
-
-    private static final int WORLD_WIDTH = 1324;
-    private static final int WORLD_HEIGHT = 1024;
+    ArrayList<Wall> walls;
 
     //values for initialization
     private final int tank1SpawnX = 100;
@@ -37,24 +42,52 @@ public class Game extends JPanel implements Runnable {
      * initial state as well.
      */
     public void initializeGame() {
-        this.world = new BufferedImage(GameConstants.GAME_SCREEN_WIDTH, GameConstants.GAME_SCREEN_HEIGHT, BufferedImage.TYPE_INT_RGB);
+        this.world = new BufferedImage(GameConstants.WORLD_WIDTH, GameConstants.WORLD_HEIGHT, BufferedImage.TYPE_INT_RGB);
         BufferedImage tank1Image = null;
         BufferedImage tank2Image = null;
         BufferedImage breakableWallImage = null;
         BufferedImage unbreakableWallImage = null;
+        walls = new ArrayList<>();
 
         try {
             //world = read(Objects.requireNonNull(Game.class.getClassLoader().getResource("Background.bmp")));
             tank1Image = read(Objects.requireNonNull(Game.class.getClassLoader().getResource("Tank1.gif")));
             tank2Image = read(Objects.requireNonNull(Game.class.getClassLoader().getResource("Tank2.gif")));
-            //breakableWallImage = read(Objects.requireNonNull(Game.class.getClassLoader().getResource("Wall1.gif")));
-            //unbreakableWallImage = read(Objects.requireNonNull(Game.class.getClassLoader().getResource("Wall2.gif")));
+            breakableWallImage = read(Objects.requireNonNull(Game.class.getClassLoader().getResource("Wall1.gif")));
+            unbreakableWallImage = read(Objects.requireNonNull(Game.class.getClassLoader().getResource("Wall2.gif")));
+            InputStreamReader isr = new InputStreamReader(Game.class.getClassLoader().getResourceAsStream("maps/map1.txt"));
+            BufferedReader mapReader = new BufferedReader(isr);
+
+            String row = mapReader.readLine();
+            if (row ==null){
+                throw new IOException("map cannot be read.");
+            }
+            String[] mapInfo = row.split("\t");
+            //get size of the map
+            int numOfCol = Integer.parseInt(mapInfo[0]);
+            int numOfRows = Integer.parseInt(mapInfo[1]);
+            //add all gameObjects to the correct spot in map
+            for(int curRow = 0; curRow <numOfRows; curRow++){
+                row = mapReader.readLine();
+                mapInfo = row.split("\t");
+                for (int curCol = 0; curCol < numOfCol; curCol++){
+                    switch (mapInfo[curCol]){
+                        case "2": //breakable wall
+                            this.walls.add(new BreakableWall(curCol*32, curRow*32, breakableWallImage));
+                            break;
+                        case "3": //unbreakable wall
+                        case "9": //outer border, not in use
+                            this.walls.add(new UnbreakableWall(curCol*32, curRow*32, unbreakableWallImage));
+                    }
+                }
+            }
+
         } catch (IOException e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
         tank1 = new Tank(tank1SpawnX, tank1SpawnY, tank1Image, 0, 0, 0);
-        tank2 = new Tank(tank2SpawnX, tank2SpawnY, tank1Image, 0, 0, 0);
+        tank2 = new Tank(tank2SpawnX, tank2SpawnY, tank2Image, 0, 0, 0);
         //create the controllers for the two tanks
         VehicleController tank1Controller = new VehicleController(tank1, KeyEvent.VK_UP, KeyEvent.VK_DOWN, KeyEvent.VK_RIGHT, KeyEvent.VK_LEFT, KeyEvent.VK_ENTER);
         VehicleController tank2Controller = new VehicleController(tank2, KeyEvent.VK_I, KeyEvent.VK_K, KeyEvent.VK_L, KeyEvent.VK_J, KeyEvent.VK_SPACE);
@@ -94,9 +127,19 @@ public class Game extends JPanel implements Runnable {
         Graphics2D g2 = (Graphics2D) graphics;
         Graphics2D buffer = world.createGraphics();
         buffer.setColor(Color.BLACK);
-        buffer.fillRect(0, 0, GameConstants.GAME_SCREEN_WIDTH, GameConstants.GAME_SCREEN_HEIGHT);
+        buffer.fillRect(0, 0, GameConstants.WORLD_WIDTH, GameConstants.WORLD_HEIGHT);
+        this.walls.forEach(wall -> wall.drawImage(buffer));
         this.tank1.drawImage(buffer);
         this.tank2.drawImage(buffer);
-        g2.drawImage(world, 0, 0, null);
+        //creates left and right screen and minimap
+        BufferedImage leftScreen = world.getSubimage(tank1.getX()-50,tank1.getY()-50,GameConstants.GAME_SCREEN_WIDTH/2, GameConstants.GAME_SCREEN_HEIGHT);
+        BufferedImage rightScreen = world.getSubimage(tank2.getX()-50, tank2.getY()-50, GameConstants.GAME_SCREEN_WIDTH/2, GameConstants.GAME_SCREEN_HEIGHT);
+        BufferedImage miniMap = world.getSubimage(0,0, GameConstants.WORLD_WIDTH, GameConstants.WORLD_HEIGHT);
+        //draw
+        g2.drawImage(leftScreen,0,0,null);
+        g2.drawImage(rightScreen, GameConstants.GAME_SCREEN_WIDTH/2, 0, null);
+        g2.scale(.10, .10);
+        g2.drawImage(miniMap,1000,1000,null);
+        //g2.drawImage(world, 0, 0, null);
     }
 }
--- src/game/GameConstants.java
+++ src/game/GameConstants.java
@@ -8,6 +8,9 @@ public class GameConstants {
     public static final int GAME_SCREEN_WIDTH = 1344; //42 columns
     public static final int GAME_SCREEN_HEIGHT = 1024; //32 rows
 
+    public static final int WORLD_WIDTH = 1984;
+    public static final int WORLD_HEIGHT = 1984;
+
 
     public static final int END_MENU_SCREEN_WIDTH = 500;
     public static final int END_MENU_SCREEN_HEIGHT = 500;
